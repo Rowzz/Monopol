130f45f31856a6bb72d085d680b2e3b28b2035a8
Tim <tim@hohengasser.it>
2018-12-27 20:38:25 +0100

    Added Class for Buildings
    # Static Reference for Next and before building

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public int ID;
+    public GameObject Next;
+    public GameObject Before;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Debug.Log(ID.ToString());
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
