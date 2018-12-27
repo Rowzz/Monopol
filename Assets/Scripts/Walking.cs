b0d49f6c1a4350372cab99e6bef6ff230443d6f6
Tim <tim@hohengasser.it>
2018-12-27 23:57:07 +0100

    movement stage #1

---
+++ Assets/Scripts/Walking.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walking : MonoBehaviour
+{
+    public GameObject Dice1;
+    public GameObject Dice2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Move()
+    {
+        
+    }
+}
