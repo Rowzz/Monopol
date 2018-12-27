bf9f8d4a5d0ad180bd37f51734a3198ccd8e7e90
Tim <tim@hohengasser.it>
2018-12-28 00:16:56 +0100

    init game controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] Dices;
+    public GameObject[] Players;
+    private int DiceResult;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        
+    }
+}
