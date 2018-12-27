a4dcd313cf21296ad389901ff10cf6803148b515
Klaus <k.striessnig@gmail.com>
2018-12-27 21:16:20 +0100

    First line implemented

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Building : MonoBehaviour
 {
-    public int ID;
     public GameObject Next;
     public GameObject Before;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        Debug.Log(ID.ToString());
     }
     // Update is called once per frame
     void Update()
