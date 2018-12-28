d99df6cfa2d47e4a1edd313fc56283b7ce498dca
Klaus <k.striessnig@gmail.com>
2018-12-28 02:18:52 +0100

    Improved moving of player

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,7 +10,7 @@ public class GameController : MonoBehaviour
     private bool PlayerMoving;
     private Vector3 toPosition;
     private int movingPositionsLeft;
-
+    private int activePlayer = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -23,36 +23,52 @@ public class GameController : MonoBehaviour
     {
         if (PlayerMoving)
         {
-            Players[0].transform.position = Vector3.MoveTowards(Players[0].transform.position, toPosition, Time.deltaTime * 2);
-            if(Players[0].transform.position == toPosition)
+            if(movePlayer(activePlayer))
             {
                 movingPositionsLeft--;
-                if (movingPositionsLeft > 0)
+                if (PlayerMoving = movingPositionsLeft > 0)
                 {
-                    toPosition = Players[0].GetComponent<PlayerFigure>().currentBuilding.Next.transform.position;
+                    setNextBuildingPosition(activePlayer);
                 }
             }
         }
         else
         {
-            bool valid = true;
-            int result = 0;
-            foreach (GameObject dice in Dices)
-            {
-                int value = (dice.GetComponent<Dice>()).diceValue;
-                result += value;
-                valid = valid && value > 0;
-            }
-
-            if (valid)//Dice rolled
+            if (validRoll(out int result))//Dice rolled
             {
                 resetDiceValues();
                 //move Player
-                movingPositionsLeft = result;
+                movingPositionsLeft = 12;
                 PlayerMoving = true;
-                toPosition = Players[0].GetComponent<PlayerFigure>().currentBuilding.Next.transform.position;
+                
+                setNextBuildingPosition(activePlayer);
             }
         }
+    }
+
+    private bool movePlayer(int playerIndex)
+    {
+        Transform PlayerTransform = Players[playerIndex].transform;
+        PlayerTransform.position = Vector3.MoveTowards(PlayerTransform.position, toPosition, Time.deltaTime*10);
+        return Vector3.Distance(PlayerTransform.position,toPosition) < 0.25;
+    }
+
+    private void setNextBuildingPosition(int playerIndex)
+    {
+        toPosition = Players[playerIndex].GetComponent<PlayerFigure>().currentBuilding.Next.transform.position;
+    }
+
+    private bool validRoll(out int result)
+    {
+        bool valid = true;
+        result = 0;
+        foreach (GameObject dice in Dices)
+        {
+            int value = (dice.GetComponent<Dice>()).diceValue;
+            result += value;
+            valid = valid && value > 0;
+        }
+        return valid;
     }
 
     private void resetDiceValues()
